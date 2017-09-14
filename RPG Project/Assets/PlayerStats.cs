7d57bb969193ff63607af3405d2846d235fa61db
arsany98 <arsanymaged13@gmail.com>
2017-09-14 22:58:15 +0200

    Stats

---
+++ RPG Project/Assets/PlayerStats.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : CharacterStats {
+
+	// Use this for initialization
+	void Start () {
+        EquipmentManager.instance.onEquipmentChanged += OnEquipmentChanged;
+	}
+	
+	void OnEquipmentChanged(Equipment newItem,Equipment oldItem)
+    {
+        if(newItem!=null)
+        {
+            armor.AddModifier(newItem.ArmorModifier);
+            damage.AddModifier(newItem.DamageModifier);
+        }
+
+        if (oldItem != null)
+        {
+            armor.RemoveModifier(oldItem.ArmorModifier);
+            damage.RemoveModifier(oldItem.DamageModifier);
+        }
+    }
+}
