5c6837f44e9be26f23db175102ce66d246de0fb3
arsany98 <arsanymaged13@gmail.com>
2017-09-29 00:44:03 +0200

    Enemy AI

---
+++ RPG Project/Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : Interactable {
+
+    public override void interact()
+    {
+        base.interact();
+
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ RPG Project/Assets/Scripts/PlayaerManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayaerManager : MonoBehaviour {
+
+    #region singleton
+    public static PlayaerManager instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("More than one instance of Inventory found!");
+            return;
+        }
+        instance = this;
+    }
+    #endregion
+
+    public GameObject player;
+}
+
